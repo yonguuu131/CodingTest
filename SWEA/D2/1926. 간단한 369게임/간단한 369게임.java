ebdae2e419ab115669d6c88357a47b5b211c3587
yonguuu131 <wo1317@naver.com>
2024-12-17 17:01:38 +0900

    [D2] Title: 간단한 369게임, Time: 107 ms, Memory: 18,252 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            int count = countClapNumber(i);
+            if(count>0){
+                for(int j=0; j<count; j++){
+                    sb.append("-");
+                }
+                sb.append(" ");
+                continue;
+            }
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+        private static int countClapNumber(int i){
+            int count = 0;
+            while(i>0){
+                int one = i%10;
+                if(one!=0 && one %3 ==0){
+                    count++;
+                }
+                i/=10;
+            }
+            return count;
+    }
+}
