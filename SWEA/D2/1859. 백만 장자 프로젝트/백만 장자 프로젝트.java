ddf4e501fb5010b05be16932580ae4b832fb9610
yonguuu131 <wo1317@naver.com>
2024-12-19 16:00:16 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 504 ms, Memory: 142,732 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc<=T; tc++){
+            sb.append("#").append(tc).append(" ");
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long[] arr = new long[n];
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            long ans = 0;
+            long max = 0;
+            for(int i=n-1; i>=0; i--){
+                if(max <= arr[i]){
+                    max = arr[i];
+                }else {
+                    ans += max - arr[i];
+                }
+            }
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
